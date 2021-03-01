235c92d8406412a8a8e20759bf51fc01956feb6a
Jason Whetton <jason@digital-arkitekt.se>
2021-03-01 11:42:36 +0100

    initial commit

---
+++ Lab01/src/Lab01.Domain.Tests/PlaylistTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+using Lab01.Domain;
+using static Lab01.Domain.Playlist;
+using System.Collections.Generic;
+using System.Collections;
+using FluentAssertions;
+
+namespace Lab01.Domain.Tests
+{
+    public class PlaylistTests
+    {
+        [Fact]
+        public void Playlist_should_be_active()
+        {
+            // arrange
+            var sut = new Playlist();
+
+            // act
+            sut.IsActive = true;
+
+            // assert
+
+            sut.IsActive.Should().BeTrue();
+        }
+    }
+}
---
+++ Lab01/src/Lab01.Domain/Playlist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab01.Domain
+{
+    public class Playlist
+    {
+        public bool IsActive { get; set; }
+    }
+}
