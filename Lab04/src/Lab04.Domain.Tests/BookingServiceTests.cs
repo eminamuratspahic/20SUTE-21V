d2e34f4e768a7188cae49933a05c687879804fed
Jason Whetton <jason@digital-arkitekt.se>
2021-03-23 09:48:22 +0100

    added lab04

---
+++ Lab04/src/Lab04.Domain.Tests/BookingServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+using MongoDB.Driver;
+using Autofac.Extras.FakeItEasy;
+
+namespace Lab04.Domain.Tests
+{
+    public class BookingServiceTests
+    {
+        [Fact]
+        public void Successful_booking_should_be_persisted()
+        {
+            // arrange
+
+            // act
+
+            // assert
+        }
+    }
+}
