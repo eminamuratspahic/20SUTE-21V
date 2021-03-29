6d762466a4b151324f3ec78c09542b6021479342
Jason Whetton <jason@digital-arkitekt.se>
2021-03-29 12:36:02 +0200

    added lab05

---
+++ Lab05/src/Lab05.Domain.Tests/BookingServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using AutoFixture.AutoFakeItEasy;
+using FakeItEasy;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab05.Domain.Tests
+{
+    public class AutofixtureTests 
+    {
+        [Fact]
+        public void Create_a_string_fixture()
+        {
+
+        }
+    }
+}
